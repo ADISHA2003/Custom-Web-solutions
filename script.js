4ac89fbe8be37902ab74427855c2b3cb34aefb2e
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-05-10 15:01:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollToTopBtn").classList.add("show");
+        } else {
+            document.getElementById("scrollToTopBtn").classList.remove("show");
+        }
+    }
+
+    function scrollToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, Opera and Edge
+    }
