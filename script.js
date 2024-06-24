f9d3e9d8f697a1f8bf557fec761832d0ae38ec76
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-06-24 18:33:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,3 @@
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, Opera and Edge
     }
-
-// Set the scroll behavior to smooth
-window.scrollToTop({
-  top: 0,
-  behavior: 'smooth'
- });
