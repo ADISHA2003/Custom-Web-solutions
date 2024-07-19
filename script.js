a17cd7887f917eaca97491ceee4fc523e14cf761
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-07-19 15:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         document.getElementById("scrollToTopBtn").classList.add("show");
     } else {
         document.getElementById("scrollToTopBtn").classList.remove("show");
